db08a15209d042865b4f7f8250e6640975c0041e
DAYNA <dayna.cho119@gmail.com>
2025-02-14 04:45:54 -0500

    Added styling to movie cards

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,9 +1,28 @@
 import { useEffect, useState } from "react";
 import { getNowPlayingMovies } from "../utilities/api";
+import { getPopularMovies } from "../utilities/api";
+import { getTopRatedMovies } from "../utilities/api";
+import { getUpcomingMovies } from "../utilities/api";
 import MovieCard from "../components/MovieCard";
+import { TMDB_IMAGE_BASE_URL } from "../globals/globalVariables";
 
 function PageHome() {
+    const [test, setTest] = useState(null);
     const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [topRatedMovies, setTopRatedMovies] = useState([]);
+    const [upcomingMovies, setUpcomingMovies] = useState([]);
+
+    useEffect(() => {
+        getPopularMovies()
+            .then((data) => {
+                setNowPlayingMovies(data.results);
+            })
+            .catch((error) => {
+                alert("Error fetching popular movies");
+            });
+    }, []);
+
     useEffect(() => {
         getNowPlayingMovies()
             .then((data) => {
@@ -17,7 +36,7 @@ function PageHome() {
 
     return (
         <main id="home">
-            <div>
+            <div className="movie-list">
                 {nowPlayingMovies.map((movieData) => (
                     <MovieCard key={movieData.id} movieData={movieData} />
                 ))}
